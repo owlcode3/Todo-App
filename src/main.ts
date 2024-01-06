100d28340ead002d5986eba70b2189a9e5534f0d
owlCode <akoladeo890@yahoo.com>
2024-01-06 19:23:29 +0100

    feat: implemented edit todo functionalities

--- src/main.ts
+++ src/main.ts
@@ -25,33 +25,41 @@ class TodoApp {
       taskLists: ".task__lists",
       textArea: "#textArea",
       cancelBtn: ".cancel",
-      showOverlayBtn: ".task__add-btn",
+      openOverlayBtn: ".task__add-btn",
       addTodoBtn: "#addTodo",
+      updateTodoBtn: "#updateTodo",
       task: ".task",
       errorEl: ".error-message"
-      // removeTodoBtn: ".remove-btn"
     });
 
     this.retrieveStorage();
     this.render();
-    this.showOverlay();
+    this.onClickOpenOverlayBtn();
     this.hideOverlay();
     this.storeTodos();
     this.deleteTodo();
+    this.editTodo();
   }
 
   hideOverlay() {
     AsHtmlElement(this.elements.cancelBtn).addEventListener("click", () => {
       AsHtmlElement(this.elements.overlay).style.display = "none";
-      AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.showOverlayBtn));
+      AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.openOverlayBtn));
+      AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value = "";
     });
   }
 
-  showOverlay() {
-    AsHtmlElement(this.elements.showOverlayBtn).addEventListener("click", () => {
-      AsHtmlElement(this.elements.overlay).style.display = "flex";
-      AsHtmlElement(this.elements.task).removeChild(AsHtmlElement(this.elements.showOverlayBtn));
-      AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).focus();
+  openOverlay() {
+    AsHtmlElement(this.elements.overlay).style.display = "flex";
+    AsHtmlElement(this.elements.task).removeChild(AsHtmlElement(this.elements.openOverlayBtn));
+    AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).focus();
+  }
+
+  onClickOpenOverlayBtn() {
+    AsHtmlElement(this.elements.openOverlayBtn).addEventListener("click", () => {
+      AsHtmlElement < HTMLButtonElement > (this.elements.addTodoBtn).style.display = "flex";
+      AsHtmlElement < HTMLButtonElement > (this.elements.updateTodoBtn).style.display = "none";
+      this.openOverlay();
     });
   }
 
@@ -60,25 +68,47 @@ class TodoApp {
       if (AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value.trim() === "") {
         AsHtmlElement(this.elements.errorEl).style.display = "block";
         return;
-      } else {
-        AsHtmlElement(this.elements.errorEl).style.display = "none";
-        AsHtmlElement(this.elements.overlay).style.display = "none";
-        AsHtmlElement(this.elements.task).appendChild(
-          AsHtmlElement(this.elements.showOverlayBtn)
-        );
-
-        this.todos = this.todos.concat({
-          id: window.crypto.randomUUID(),
-          text: AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value,
-          completed: false
-        });
       }
+      AsHtmlElement(this.elements.errorEl).style.display = "none";
+      AsHtmlElement(this.elements.overlay).style.display = "none";
+      AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.openOverlayBtn));
+
+      this.todos = this.todos.concat({
+        id: window.crypto.randomUUID(),
+        text: AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value,
+        completed: false
+      });
 
       AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value = "";
 
       this.populateStorage();
       this.render();
     });
+  }
+
+  storeEditedTodo(updatedText: string, id: string) {
+    if (updatedText.trim() === "") {
+      AsHtmlElement(this.elements.errorEl).style.display = "block";
+      return;
+    }
+
+    AsHtmlElement(this.elements.errorEl).style.display = "none";
+    AsHtmlElement(this.elements.overlay).style.display = "none";
+    AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.openOverlayBtn));
+
+    this.todos = this.todos.map(todo => {
+      return todo.id === id ?
+        {
+          ...todo,
+          text: updatedText
+        } :
+        todo;
+    });
+
+    AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value = "";
+
+    this.populateStorage();
+    this.render();
   }
 
   deleteTodo() {
@@ -98,7 +128,32 @@ class TodoApp {
   }
 
   editTodo() {
+    AsHtmlElement(this.elements.taskLists).addEventListener("click", event => {
+      const target = event.target as HTMLElement;
 
+      if (target.classList.contains("edit-btn")) {
+        const nextBtnEl = target.nextElementSibling as HTMLButtonElement;
+        const id = nextBtnEl.dataset.id!;
+        const todo = this.todos.find(todo => todo.id === id);
+
+        if (todo?.text) {
+          AsHtmlElement < HTMLButtonElement > (this.elements.updateTodoBtn).style.display = "flex";
+          AsHtmlElement < HTMLButtonElement > (this.elements.addTodoBtn).style.display = "none";
+          this.openOverlay();
+          AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value = todo?.text!;
+        }
+
+        AsHtmlElement < HTMLTextAreaElement > (this.elements.updateTodoBtn).addEventListener(
+          "click",
+          () => {
+            this.storeEditedTodo(
+              AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value,
+              id
+            );
+          }
+        );
+      }
+    });
   }
 
   populateStorage() {
@@ -122,10 +177,10 @@ class TodoApp {
 
     if (this.todos.length === 0) {
       const el = /*html*/ `
-                <h2 class="empty-todo">
-                  Add todos to see your list of todos here.
-                </h2>
-                `;
+                                  <h2 class="empty-todo">
+                                    Add todos to see your list of todos here.
+                                  </h2>
+                                  `;
 
       AsHtmlElement(this.elements.taskLists).innerHTML = el;
       return;
@@ -138,20 +193,20 @@ class TodoApp {
       month: string,
       year: number
     ) => /*html*/ `
-          <li class="task__list">
-            <p class="task__texts">${text}</p>
-            <div class="task__added">
-              <div class="task__date">
-                <span>Added on</span>
-                <span>:</span>
-                <span>${day}/${month}/${year}</span>
-              </div>
-              <div class="edit-box">
-                <button class="edit-btn">Edit</button>
-                <button data-id="${id}" class="remove-btn">Remove</button>
-              </div>
-            </div>
-          </li>`;
+                            <li class="task__list">
+                              <p class="task__texts">${text}</p>
+                              <div class="task__added">
+                                <div class="task__date">
+                                  <span>Added on</span>
+                                  <span>:</span>
+                                  <span>${day}/${month}/${year}</span>
+                                </div>
+                                <div class="edit-box">
+                                  <button class="edit-btn">Edit</button>
+                                  <button data-id="${id}" class="remove-btn">Delete</button>
+                                </div>
+                              </div>
+                            </li>`;
 
     const todos = this.todos.map(({
       text,
