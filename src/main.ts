35908e926006519566d4086e3a0a987a9d3c48e0
owlCode <akoladeo890@yahoo.com>
2024-01-06 13:18:11 +0100

    feat: made some adjustment to the css and html file

--- src/main.ts
+++ src/main.ts
@@ -1,137 +1,143 @@
 import AsHtmlElement from "./utils/asHtmlElement";
-import { mapElements } from "./utils/dom";
+import {
+  mapElements
+} from "./utils/dom";
 
 interface Todo {
-   id: string;
-   text: string;
-   completed: boolean;
+  id: string;
+  text: string;
+  completed: boolean;
 }
 
 class TodoApp {
-   element: string | HTMLElement;
+  element: string | HTMLElement;
 
-   elements: {
-      [K in keyof ReturnType<typeof mapElements>]: Element | NodeList | null;
-   } = {};
+  elements: {
+    [K in keyof ReturnType < typeof mapElements > ]: Element | NodeList | null;
+  } = {};
 
-   todos: Todo[] = [];
+  todos: Todo[] = [];
 
-   constructor(element: any) {
-      this.element = element;
-      this.elements = mapElements(this.element, {
-         overlay: ".overlay",
-         taskLists: ".task__lists",
-         textArea: "#textArea",
-         cancelBtn: ".cancel",
-         showOverlayBtn: ".task__add-btn",
-         addTodoBtn: "#addTodo",
-         task: ".task",
-         errorEl: ".error-message"
-         // removeTodoBtn: ".remove-btn"
-      });
+  constructor(element: any) {
+    this.element = element;
+    this.elements = mapElements(this.element, {
+      overlay: ".overlay",
+      taskLists: ".task__lists",
+      textArea: "#textArea",
+      cancelBtn: ".cancel",
+      showOverlayBtn: ".task__add-btn",
+      addTodoBtn: "#addTodo",
+      task: ".task",
+      errorEl: ".error-message"
+      // removeTodoBtn: ".remove-btn"
+    });
 
-      this.retrieveStorage();
-      this.render();
-      this.showOverlay();
-      this.hideOverlay();
-      this.storeTodos();
-      this.deleteTodo();
-   }
+    this.retrieveStorage();
+    this.render();
+    this.showOverlay();
+    this.hideOverlay();
+    this.storeTodos();
+    this.deleteTodo();
+  }
 
-   hideOverlay() {
-      AsHtmlElement(this.elements.cancelBtn).addEventListener("click", () => {
-         AsHtmlElement(this.elements.overlay).style.display = "none";
-         AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.showOverlayBtn));
-      });
-   }
+  hideOverlay() {
+    AsHtmlElement(this.elements.cancelBtn).addEventListener("click", () => {
+      AsHtmlElement(this.elements.overlay).style.display = "none";
+      AsHtmlElement(this.elements.task).appendChild(AsHtmlElement(this.elements.showOverlayBtn));
+    });
+  }
 
-   showOverlay() {
-      AsHtmlElement(this.elements.showOverlayBtn).addEventListener("click", () => {
-         AsHtmlElement(this.elements.overlay).style.display = "flex";
-         AsHtmlElement(this.elements.task).removeChild(AsHtmlElement(this.elements.showOverlayBtn));
-         AsHtmlElement<HTMLTextAreaElement>(this.elements.textArea).focus();
-      });
-   }
+  showOverlay() {
+    AsHtmlElement(this.elements.showOverlayBtn).addEventListener("click", () => {
+      AsHtmlElement(this.elements.overlay).style.display = "flex";
+      AsHtmlElement(this.elements.task).removeChild(AsHtmlElement(this.elements.showOverlayBtn));
+      AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).focus();
+    });
+  }
 
-   storeTodos() {
-      AsHtmlElement(this.elements.addTodoBtn).addEventListener("click", _ => {
-         if (AsHtmlElement<HTMLTextAreaElement>(this.elements.textArea).value.trim() === "") {
-            AsHtmlElement(this.elements.errorEl).style.display = "block";
-            return;
-         } else {
-            AsHtmlElement(this.elements.errorEl).style.display = "none";
-            AsHtmlElement(this.elements.overlay).style.display = "none";
-            AsHtmlElement(this.elements.task).appendChild(
-               AsHtmlElement(this.elements.showOverlayBtn)
-            );
+  storeTodos() {
+    AsHtmlElement(this.elements.addTodoBtn).addEventListener("click", _ => {
+      if (AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value.trim() === "") {
+        AsHtmlElement(this.elements.errorEl).style.display = "block";
+        return;
+      } else {
+        AsHtmlElement(this.elements.errorEl).style.display = "none";
+        AsHtmlElement(this.elements.overlay).style.display = "none";
+        AsHtmlElement(this.elements.task).appendChild(
+          AsHtmlElement(this.elements.showOverlayBtn)
+        );
 
-            this.todos = this.todos.concat({
-               id: window.crypto.randomUUID(),
-               text: AsHtmlElement<HTMLTextAreaElement>(this.elements.textArea).value,
-               completed: false
-            });
-         }
-
-         AsHtmlElement<HTMLTextAreaElement>(this.elements.textArea).value = "";
-
-         this.populateStorage();
-         this.render();
-      });
-   }
-
-   deleteTodo() {
-      document.addEventListener("click", event => {
-         const target = event.target as HTMLElement;
-
-         if (target.closest && target.closest(".task__lists")) {
-            if (target.classList.contains("remove-btn")) {
-               const id = target.dataset.id!;
-               this.todos = this.todos.filter(todo => todo.id !== id);
-
-               this.populateStorage();
-               this.render();
-            }
-         }
-      });
-   }
-
-   populateStorage() {
-      localStorage.setItem("todos", JSON.stringify(this.todos));
-   }
-
-   retrieveStorage() {
-      if (!localStorage.getItem("todos")) {
-         return;
+        this.todos = this.todos.concat({
+          id: window.crypto.randomUUID(),
+          text: AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value,
+          completed: false
+        });
       }
 
-      this.todos = this.todos.concat(JSON.parse(localStorage.getItem("todos")!));
-   }
+      AsHtmlElement < HTMLTextAreaElement > (this.elements.textArea).value = "";
 
-   render() {
-      const currentDate = new Date();
+      this.populateStorage();
+      this.render();
+    });
+  }
 
-      const day = currentDate.getDate().toString().padStart(2, "0");
-      const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-      const year = currentDate.getFullYear();
+  deleteTodo() {
+    document.addEventListener("click", event => {
+      const target = event.target as HTMLElement;
 
-      if (this.todos.length === 0) {
-         const el = /*html*/ `
+      if (target.closest && target.closest(".task__lists")) {
+        if (target.classList.contains("remove-btn")) {
+          const id = target.dataset.id!;
+          this.todos = this.todos.filter(todo => todo.id !== id);
+
+          this.populateStorage();
+          this.render();
+        }
+      }
+    });
+  }
+
+  editTodo() {
+
+  }
+
+  populateStorage() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  retrieveStorage() {
+    if (!localStorage.getItem("todos")) {
+      return;
+    }
+
+    this.todos = this.todos.concat(JSON.parse(localStorage.getItem("todos") !));
+  }
+
+  render() {
+    const currentDate = new Date();
+
+    const day = currentDate.getDate().toString().padStart(2, "0");
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+    const year = currentDate.getFullYear();
+
+    if (this.todos.length === 0) {
+      const el = /*html*/ `
                 <h2 class="empty-todo">
                   Add todos to see your list of todos here.
                 </h2>
                 `;
 
-         AsHtmlElement(this.elements.taskLists).innerHTML = el;
-         return;
-      }
+      AsHtmlElement(this.elements.taskLists).innerHTML = el;
+      return;
+    }
 
-      const listEl = (
-         text: string,
-         id: string,
-         day: string,
-         month: string,
-         year: number
-      ) => /*html*/ `
+    const listEl = (
+      text: string,
+      id: string,
+      day: string,
+      month: string,
+      year: number
+    ) => /*html*/ `
           <li class="task__list">
             <p class="task__texts">${text}</p>
             <div class="task__added">
@@ -147,10 +153,13 @@ class TodoApp {
             </div>
           </li>`;
 
-      const todos = this.todos.map(({ text, id }) => listEl(text, id, day, month, year)).join("");
+    const todos = this.todos.map(({
+      text,
+      id
+    }) => listEl(text, id, day, month, year)).join("");
 
-      AsHtmlElement(this.elements.taskLists).innerHTML = todos;
-   }
+    AsHtmlElement(this.elements.taskLists).innerHTML = todos;
+  }
 }
 
 new TodoApp("body");
